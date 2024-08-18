df6d430555c73950802eacfacca6eb1bbd10b68c
MartinV <valleixmartin89@gmail.com>
2024-08-18 13:25:20 +0200

    Gravity Final Modification

--- Assets/Scripts/Sun.cs
+++ Assets/Scripts/Sun.cs
@@ -1,6 +1,8 @@
 public class Sun : Celestial
 {
     public float gravitationalConstant = 10f;
+    public float gravitationalCorrectionForce = 1f;
+    public float orbitalVelocityThreshold = 5f;
     
     public override float GetTotalDistanceTraveled()
     {
